f228e6302bf26143e4946ce37b8d9f948d2b474d
Abdelillah <derrarabdelillah55@gmail.com>
2025-05-29 16:48:30 -0700

    Complete 70% of the project

---
+++ src/script.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+//   spaceBetween: 100,
+  loop: true,
+  spaceBetween: 10,
+  grabCursor: true,
+
+  autoplay:{
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+
+    620: {
+        slidesPerView: 2
+    },
+
+    1024: {
+        slidesPerView: 3
+    },
+  }
+});
